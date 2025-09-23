1d6d930119f347c389a6391b0f56362e58906ac4
deepa-sh04 <deepasharma4421@gmail.com>
2025-09-23 17:31:35 +0530

    Create Ransom_Note.py

---
+++ Ransom_Note.py
@@ -0,0 +1,21 @@
+"""
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+"""
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        dict={}
+        for char in magazine:
+
+          if char not in dict:
+            dict[char]=1
+          else:
+            dict[char]+=1
+        for char in ransomNote:
+            if char in dict and dict[char]>0:
+                dict[char]-=1
+            else:
+                return False
+        return True                   
